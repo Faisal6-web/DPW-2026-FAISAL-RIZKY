ea697588162c562fd2fac0605f9634d0904b1df5
Faisal6-web <rizkyfaisal886@gmail.com>
2026-09-23 04:56:11 +0700

    list-php

---
+++ Jobsheet7/buku/list.php
@@ -0,0 +1,12 @@
+<?php
+$page_title = "Daftar Buku"
+include __DIR__ . '/../includes/header.php';
+
+$flash = $_SESSION['flash'] ?? null;
+unset($_SESSION['flash']);
+$daftarBuku = $_SESSION['buku'] ?? [];
+?>
+
+    <section>
+        
+    </section>
